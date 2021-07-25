c1560e4f19a9bd5543901b480a9c2fca8d13344f
daoudtaha <daoudtaha@gmail.com>
2021-07-25 12:26:45 +0100

    Initialisation

---
+++ sagetools/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnValide = New System.Windows.Forms.Button()
+        Me.labNO = New System.Windows.Forms.Label()
+        Me.txtNO = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnValide)
+        Me.GroupBox1.Controls.Add(Me.labNO)
+        Me.GroupBox1.Controls.Add(Me.txtNO)
+        Me.GroupBox1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(297, 115)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ANNULATION TICKET"
+        '
+        'btnValide
+        '
+        Me.btnValide.Location = New System.Drawing.Point(235, 44)
+        Me.btnValide.Name = "btnValide"
+        Me.btnValide.Size = New System.Drawing.Size(44, 33)
+        Me.btnValide.TabIndex = 3
+        Me.btnValide.Text = "OK"
+        Me.btnValide.UseVisualStyleBackColor = True
+        '
+        'labNO
+        '
+        Me.labNO.AutoSize = True
+        Me.labNO.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labNO.Location = New System.Drawing.Point(6, 51)
+        Me.labNO.Name = "labNO"
+        Me.labNO.Size = New System.Drawing.Size(124, 19)
+        Me.labNO.TabIndex = 2
+        Me.labNO.Text = "Numero Ticket :"
+        '
+        'txtNO
+        '
+        Me.txtNO.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNO.Location = New System.Drawing.Point(136, 48)
+        Me.txtNO.MaxLength = 8
+        Me.txtNO.Name = "txtNO"
+        Me.txtNO.Size = New System.Drawing.Size(93, 27)
+        Me.txtNO.TabIndex = 1
+        Me.txtNO.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(321, 136)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "SAGE TOOLS"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnValide As System.Windows.Forms.Button
+    Friend WithEvents labNO As System.Windows.Forms.Label
+    Friend WithEvents txtNO As System.Windows.Forms.TextBox
+
+End Class
---
+++ sagetools/Form1.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Data.SqlClient
+Imports MadMilkman.Ini
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Public cnxStr As String = ""
+
+    'Public Function ConnectToSQL() As String
+    '    Dim con As New SqlConnection
+    '    Dim reader As SqlDataReader
+    '    Try
+    '        con.ConnectionString = cnxStr
+    '        Dim cmd As New SqlCommand("SELECT * FROM sys.tables", con)
+    '        con.Open()
+    '        Console.WriteLine("Connection Opened")
+    '        reader = cmd.ExecuteReader()
+    '        While reader.Read
+    '            Console.WriteLine(String.Format("{0}, {1}", reader(0), reader(1)))
+    '        End While
+    '    Catch ex As Exception
+    '        MessageBox.Show("Error while connecting to SQL Server." & ex.Message)
+    '    Finally
+    '        con.Close()
+    '    End Try          '
+    '    Return "done"
+    'End Function
+
+
+    Public Function getListOfGO(ByVal NT As String) As List(Of String)
+        Dim SQL As String = "SELECT RG_No FROM F_REGLECH WHERE DO_Piece = '" & NT & "' AND DO_Type = '30'"
+        Dim output As New List(Of String)()
+
+        Using cn = New SqlConnection(cnxStr)
+            Using cmd = New SqlCommand(SQL, cn)
+                cn.Open()
+                Try
+                    Dim dr = cmd.ExecuteReader()
+                    While dr.Read()
+                        output.Add(dr("RG_No").ToString())
+                    End While
+                Catch e As SqlException
+                    MessageBox.Show("There was an error accessing your data. DETAIL: " & e.ToString())
+                End Try
+            End Using
+        End Using
+
+        Return output
+
+    End Function
+
+
+    Private Sub AnnulationTicket(ByVal NT As String)
+
+        Dim allRG As New List(Of String)()
+        allRG = getListOfGO(NT)
+
+        Using connection As New SqlConnection(cnxStr)
+            connection.Open()
+
+            Dim command As SqlCommand = connection.CreateCommand()
+            Dim transaction As SqlTransaction
+
+            transaction = connection.BeginTransaction("AnnulationTicket")
+            command.Connection = connection
+            command.Transaction = transaction
+
+            Dim strSQL As String
+            Try
+                strSQL = "DELETE FROM F_REGLECH WHERE DO_Piece = '" & NT.Trim & "' AND DO_Type = '30'"
+                EcrireTXT(strSQL)
+                command.CommandText = strSQL
+                command.ExecuteNonQuery()
+
+                For Each g As String In allRG
+                    strSQL = "DELETE FROM F_CREGLEMENT WHERE RG_No ='" & g.Trim & "' AND DO_Type = '30'"
+                    EcrireTXT(strSQL)
+                    command.CommandText = strSQL
+                    command.ExecuteNonQuery()
+                Next
+
+                strSQL = "DELETE FROM F_DOCREGL WHERE DO_Piece = '" & NT.Trim & "' AND DO_Type = '30'"
+                EcrireTXT(strSQL)
+                command.CommandText = strSQL
+                command.ExecuteNonQuery()
+                strSQL = "UPDATE F_DOCENTETE SET DO_VALIDE =0 where DO_Piece='" & NT.Trim & "' AND DO_TYPE ='30'"
+                EcrireTXT(strSQL)
+                command.CommandText = strSQL
+                command.ExecuteNonQuery()
+                strSQL = "UPDATE F_DOCENTETE SET DO_Attente=1 where DO_Piece='" & NT.Trim & "' AND DO_TYPE ='30'"
+                EcrireTXT(strSQL)
+                command.CommandText = strSQL
+                command.ExecuteNonQuery()
+
+                transaction.Commit()
+                EcrireTXT("Transaction Commited.")
+                MsgBox("Ticket Annuler.")
+                txtNO.Text = ""
+
+            Catch ex As Exception
+                'Console.WriteLine("Commit Exception Type: " & ex.GetType().ToString & "  Message: " & ex.Message)
+                EcrireTXT("Commit Exception Type: " & ex.GetType().ToString & "  Message: " & ex.Message)
+                Try
+                    transaction.Rollback()
+                    EcrireTXT("Transaction Rollback.")
+                Catch ex2 As Exception
+
+                    EcrireTXT("Commit Exception Type: " & ex.GetType().ToString & "  Message: " & ex.Message)
+                    MsgBox("Erreur Transaction voir fichier LOG")
+                End Try
+            End Try
+        End Using
+    End Sub
+
+    Private Sub txtNO_KeyDown(sender As Object, e As KeyEventArgs) Handles txtNO.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnValide_Click(sender, e)
+        End If
+    End Sub
+
+    Private Sub btnValide_Click(sender As Object, e As EventArgs) Handles btnValide.Click
+        Dim noticket As Integer = 0
+        If Integer.TryParse(txtNO.Text, noticket) Then
+            If noticket > 0 Then
+                'MsgBox(noticket)
+                'ConnectToSQL()
+                EcrireTXT("#============ " & Now.ToString("yyyy-MM-dd HH:mm:ss") & " : Debut Annulation Ticket Numero " & noticket)
+                AnnulationTicket(noticket)
+            Else
+                MsgBox("Merci de verifier le numero")
+            End If
+        Else
+            MsgBox("Merci de verifier le numero")
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim appPath As String = My.Application.Info.DirectoryPath
+        Dim ini As New IniFile()
+        ini.Load(appPath & "\CONFIG.INI")
+        cnxStr = ini.Sections("DATABASE").Keys("CONNEXION").Value
+        cnxStr = Regex.Replace(cnxStr, "[""]", String.Empty)
+    End Sub
+
+    Public Sub EcrireTXT(ByVal txt As String)
+        Try
+            Dim FILE_NAME As String = Application.StartupPath & "\HISTORIE.LOG"
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME, True)
+            objWriter.WriteLine(txt)
+            objWriter.Close()
+        Catch ex As Exception
+            EcrireTXT(Now.ToString("HHmm : ") & "Erreur EcrireTXT : " & changeCRLF(ex.Message))
+        End Try
+    End Sub
+
+    Function changeCRLF(ByVal txt As String) As String
+        Return Replace(Replace(Replace(txt, vbCrLf, "<CRLF>"), vbLf, "<LF>"), vbCr, "<CR>")
+    End Function
+
+
+
+End Class
